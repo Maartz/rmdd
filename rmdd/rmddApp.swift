3816d02967d82d4a29f922d0ff44554aa89cd57b
William Martin <maartz@icloud.com>
2023-01-06 09:11:32 +0100

    Initial Commit

---
+++ rmdd/rmddApp.swift
@@ -0,0 +1,17 @@
+//
+//  rmddApp.swift
+//  rmdd
+//
+//  Created by Maartz on 06/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct rmddApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

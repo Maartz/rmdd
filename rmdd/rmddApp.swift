a73c01792992960a47264ccfff02096b83f6f2a7
William Martin <maartz@icloud.com>
2023-01-06 15:23:12 +0100

    It works

--- rmdd/rmddApp.swift
+++ rmdd/rmddApp.swift
@@ -1,6 +1,6 @@
 //
-//  rmddApp.swift
-//  rmdd
+//  RMDDApp.swift
+//  RMDD
 //
 //  Created by Maartz on 06/01/2023.
 //
@@ -8,10 +8,22 @@
 import SwiftUI
 
 @main
-struct rmddApp: App {
+struct RMDDApp: App {
+    let derivedDataPath = "\(NSHomeDirectory())/Library/Developer/Xcode/DerivedData"
+
+    @State var name: String = "RMDD"
+
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        MenuBarExtra(name, systemImage: "toilet.fill") {
+            Button("Clean derived data") {
+                try? FileManager.default.removeItem(atPath: derivedDataPath)
+            }
+            .keyboardShortcut("d")
+
+            Divider()
+            Button("Quit") {
+                NSApplication.shared.terminate(nil)
+            }.keyboardShortcut("q")
         }
     }
 }
